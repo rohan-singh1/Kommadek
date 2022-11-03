0b5b1ce2737915d4f52fa6892c1031d21d585083
Rohan Singh <vicky94rohan@gmail.com>
2022-11-03 14:01:51 -0700

    CSV parser functionality implemented

--- include/csvparser.h
+++ include/csvparser.h
@@ -2,12 +2,21 @@
 #define CSVPARSER_H
 
 #include <QObject>
+#include <QFile>
+#include <QTextStream>
 
 class CsvParser : public QObject
 {
     Q_OBJECT
+
+private:
+    QStringList _allLines;
+
 public:
     explicit CsvParser(QObject *parent = nullptr);
+
+    QStringList readAllLines(QString file_name_with_path);
+    QList <QStringList> parseCsvData(QStringList csv_lines);
 
 signals:
 
--- src/csvparser.cpp
+++ src/csvparser.cpp
@@ -4,3 +4,34 @@ CsvParser::CsvParser(QObject *parent) : QObject(parent)
 {
 
 }
+
+QStringList CsvParser::readAllLines(QString file_name_with_path)
+{
+    QFile file(file_name_with_path);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        return _allLines;
+    }
+
+    QTextStream in(&file);
+    while (!in.atEnd())
+    {
+        QString line = in.readLine();
+        _allLines.append(line);
+    }
+    return _allLines;
+}
+
+QList<QStringList> CsvParser::parseCsvData(QStringList csv_lines)
+{
+    QList <QStringList> parsedData;
+
+    QString cell;
+    foreach (cell, csv_lines)
+    {
+        QStringList row = cell.split(u',', Qt::SkipEmptyParts);
+        parsedData.append(row);
+    }
+
+    return parsedData;
+}
