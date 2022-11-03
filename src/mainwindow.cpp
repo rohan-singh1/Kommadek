b178a38d046882c9f41eb610a9b1574300441ed7
Rohan Singh <vicky94rohan@gmail.com>
2022-11-03 14:03:28 -0700

    Functionality to load and view CSV files implemented, functions for Open and Quit buttons and their corresponding menu items implemented

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->ui->stackedWidget->setCurrentIndex(0);
 
 
 #ifdef USE_TEST_DATA_1
@@ -29,6 +30,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     KommaTableModel *tableModel =  new KommaTableModel(records, this);
     this->ui->tableView->setModel(tableModel);
+    this->ui->stackedWidget->setCurrentIndex(1);
 
 #endif
 
@@ -39,6 +41,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     KommaTableModel *tableModel =  new KommaTableModel(stringMatrix, this);
     this->ui->tableView->setModel(tableModel);
+    this->ui->stackedWidget->setCurrentIndex(1);
 
 #endif
 
@@ -47,5 +50,62 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,tr("Open CSV File"), "", tr("CSV Files (*.csv)"));
+
+    CsvParser *csvParser = new CsvParser(this);
+    auto lines = csvParser->readAllLines(fileName);
+    auto stringMatrix  = csvParser->parseCsvData(lines);
+    KommaTableModel *tableModel =  new KommaTableModel(stringMatrix, this);
+
+    if(!stringMatrix.empty())
+    {
+
+        this->ui->tableView->setModel(tableModel);
+        this->ui->stackedWidget->setCurrentIndex(1);
+    }
+
+    QString statusMessage = "Rows: " +
+        QString::number(tableModel->rowCount()) + " | Columns: " +
+        QString::number(tableModel->columnCount()) + " | Total Cells: " +
+        QString::number(tableModel->rowCount() * tableModel->columnCount()) + " | Filled Cells: " +
+        QString::number(tableModel->cellCount()) + " | Empty Cells: " +
+        QString::number((tableModel->rowCount() * tableModel->columnCount()) - tableModel->cellCount());
+
+    ui->statusbar->showMessage(statusMessage);
+}
+
+
+void MainWindow::on_actionQuit_triggered()
+{
+    exit(0);
+}
+
+
+void MainWindow::on_actionHelp_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionAbout_triggered()
+{
+
+}
+
+
+void MainWindow::on_openButton_clicked()
+{
+    on_actionOpen_triggered();
+}
+
+
+void MainWindow::on_quitButton_clicked()
+{
+    exit(0);
 }
 
