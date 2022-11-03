004d7faee96fa6581229b4dc94286ffb0f19163f
Rohan Singh <vicky94rohan@gmail.com>
2022-11-02 17:31:46 -0700

    Table View widget populated by custom table model constructed using test data; two sets of test data provided for review

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,6 +6,42 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+#ifdef USE_TEST_DATA_1
+
+    // Test Data #1 as parser is not implemented yet
+    QList <QStringList> records;
+
+    int numRows = 25;
+    int numColumns = 25;
+
+    for (int i = 1; i <= numRows; i++)
+    {
+        QStringList row;
+        for (int j = 1; j <= numColumns; j++)
+        {
+            QString cellValue = QString::number(i) + ", " + QString::number(j);
+            row.append(cellValue);
+        }
+        records.append(row);
+    }
+
+    KommaTableModel *tableModel =  new KommaTableModel(records, this);
+    this->ui->tableView->setModel(tableModel);
+
+#endif
+
+#ifdef USE_TEST_DATA_2
+
+    // Test Data #2 as parser is not implemented yet
+    QList <QStringList> stringMatrix  = {{"A", "B", "C", "D"}, {"Alpha", "Bravo", "Charlie", "Delta"}, {"1", "2", "3", "4", "5"}};
+
+    KommaTableModel *tableModel =  new KommaTableModel(stringMatrix, this);
+    this->ui->tableView->setModel(tableModel);
+
+#endif
+
 }
 
 MainWindow::~MainWindow()
