5f131d52713a140d5c5c4934d71b3c78465eef67
zarrinpsu <monirzadehzarrin@gmail.com>
2022-11-27 01:12:05 -0800

    Help dialog added

---
+++ src/helpdialog.cpp
@@ -0,0 +1,20 @@
+#include "include/helpdialog.h"
+#include "ui_helpdialog.h"
+
+HelpDialog::HelpDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::HelpDialog)
+{
+    ui->setupUi(this);
+}
+
+HelpDialog::~HelpDialog()
+{
+    delete ui;
+}
+
+void HelpDialog::on_buttonBox_2_accepted()
+{
+    close();
+}
+
